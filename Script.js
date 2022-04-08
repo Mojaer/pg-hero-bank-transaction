5a487ead58995e4c9fa9f29f48daec3dde330e2f
mojaer <mojaermohi@gmail>
2022-04-09 01:01:11 +0600

    final code

--- Script.js
+++ Script.js
@@ -1,8 +1,48 @@
-// console.log('hello')
+const subBtn = document.getElementById('sub-btn');
+const logIn = document.getElementById('log-in');
+const transaction = document.getElementById('transaction');
 
-// const subBtn = document.getElementById('sub-btn')
+subBtn.addEventListener('click', function() {
+    logIn.style.display = 'none';
+    transaction.style.display = 'block';
+})
 
-// subBtn.addEventListener('click',function(){
-//     document.getElementById('log-in').style.display = 'none' ;
-//     document.getElementById('transaction').style.display = 'block' ;
-// })
+//  transaction calculation
+
+const withdraw = document.getElementById('withdraw');
+const deposit = document.getElementById('deposit');
+const balance = document.getElementById('balance');
+const depositInput = document.getElementById('depositInput');
+const withdrawInput = document.getElementById('withdrawInput');
+const depositBtn = document.getElementById('depositBtn');
+const withdrawBtn = document.getElementById('withdrawBtn');
+
+depositBtn.addEventListener('click', function() {
+    deposit.innerText = parseFloat(depositInput.value) + parseFloat(deposit.innerText);
+
+    balance.innerText = parseFloat(balance.innerText) + parseFloat(depositInput.value);
+    depositInput.value = 0;
+})
+
+withdrawBtn.addEventListener('click', function() {
+    withdraw.innerText = parseFloat(withdrawInput.value) + parseFloat(withdraw.innerText);
+
+    balance.innerText = parseFloat(balance.innerText) - parseFloat(withdrawInput.value);
+    withdrawInput.value = 0;
+})
+
+// final transaction
+const finalDeposit = document.getElementById('finalDeposit');
+
+const finalWithdraw = document.getElementById('finalWithdraw');
+
+const finalBalance = document.getElementById('finalBalance');
+
+document.getElementById('finalTransaction').addEventListener('click', function() {
+    finalWithdraw.innerText = withdraw.innerText;
+    finalDeposit.innerText = deposit.innerText;
+    finalBalance.innerText = balance.innerText;
+
+    transaction.style.display = 'none';
+    document.getElementById('endTransaction').style.display = 'block';
+})
