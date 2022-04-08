89f624927192b6b21410575a13d8747a18be6b23
mojaer <mojaermohi@gmail>
2022-04-08 16:52:39 +0600

    design submit

---
+++ Script.js
@@ -0,0 +1,8 @@
+// console.log('hello')
+
+// const subBtn = document.getElementById('sub-btn')
+
+// subBtn.addEventListener('click',function(){
+//     document.getElementById('log-in').style.display = 'none' ;
+//     document.getElementById('transaction').style.display = 'block' ;
+// })
